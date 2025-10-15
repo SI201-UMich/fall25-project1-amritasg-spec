f713e3d091f74606fc4b3cb8aa2581a8213b0037
Amrita Gujarati <amritagujarati@Amritas-MacBook-Air-7.local>
2025-10-15 17:32:51 -0400

    added the intro with names, collaborators, and AI statement

---
+++ project1_code.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------
+# Name: Amrita Gujarati 
+# Student ID: 26505868
+# Email: amritasg@umich.edu
+# Collaborators: Amrita Gujarati, Anna DeWitt, Willow Tonelli
+# AI Tools Used: ChatGPT (for debugging guidance and code review)
+# Functions created by Anna:
+# Functions created by Amrita:
+# Functions created by Willow:
+# Dataset: Palmer Penguins (Kaggle)
+# -------------------------------------------------------------
+
