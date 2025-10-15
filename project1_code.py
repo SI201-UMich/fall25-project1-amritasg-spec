0622055d222f268cb25787ea3daf96b21a51fca2
Amrita Gujarati <amritagujarati@Amritas-MacBook-Air-7.local>
2025-10-15 17:35:45 -0400

    avg flipper and bill length functions (3 and 4)

--- project1_code.py
+++ project1_code.py
@@ -22,3 +22,22 @@ def load_penguin(csv_file):
 
 def get_length(penguins, species):
   return [penguin['bill_length_mm'] for penguin in penguins if penguin['species'] == species.lower()]
+
+def avg_bill_length_by_sex(penguins):
+    species_list = ['Adelie', 'Chinstrap', 'Gentoo']
+    averages = {}
+    for species in species_list:
+        for sex in ['male', 'female']:
+            lengths = [float(p['bill_length_mm']) for p in penguins
+                       if p['species'] == species and p['sex'] == sex]
+            if lengths:
+                averages[f"{species}_{sex}"] = sum(lengths) / len(lengths)
+    return averages
+
+def avg_flipper_length(penguins):
+  species_list = ['adelie', 'chinstrap', 'gentoo']
+  averages = {}
+  for species in species_list:
+    lengths = get_length(penguins, species)
+    averages[species] = sum(map(float, lengths)) / len(lengths)
+  return averages
